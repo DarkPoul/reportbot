a02a68df6be92b4354ecc0d69676dde85082d9f3
DarkPoul <poulpotter1@gmail.com>
2026-02-24 12:05:42 +0200

    initial

---
+++ src/main/java/esvar/ua/reportbot/ReportbotApplication.java
@@ -0,0 +1,13 @@
+package esvar.ua.reportbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportbotApplication.class, args);
+    }
+
+}
